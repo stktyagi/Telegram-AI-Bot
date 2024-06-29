8ff3f6cc85b06e4a003ce53f0a2588b0a4e9234d
stktyagi <142912014+stktyagi@users.noreply.github.com>
2024-06-29 14:20:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,9 +20,29 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     "response_mime_type": "text/plain",
     }
 
+    safety_settings = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_NONE"
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_NONE"
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_NONE"
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_NONE"
+    },
+    ]
+
     model = genai.GenerativeModel(
     model_name="gemini-1.5-flash",
     generation_config=generation_config,
+    safety_settings=safety_settings
     )
 
     chat_session = model.start_chat(
